890cef62777028a529b4934f1a978787d4359b38
Kresno Nugroho <kresnonugroho46@gmail.com>
2025-12-09 10:27:06 +0700

    update:about

--- src/components/organisms/AboutUs.tsx
+++ src/components/organisms/AboutUs.tsx
@@ -88,7 +88,11 @@ function AboutUs() {
                                 {t('trustedPartner')}
                             </h3>
                             <div className="space-y-4 text-[#080031]/90 leading-relaxed text-lg">
-                                <p>{t('aboutText')}</p>
+                                {t('aboutText')
+                                .split('\n')
+                                .map((line, idx) => (
+                                    <p key={idx}>{line}</p>
+                                ))}
                             </div>
                         </div>
 
