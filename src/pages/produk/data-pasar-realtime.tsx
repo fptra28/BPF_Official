025fdc397faea478b8870eb16d6a0ebc62e5c13d
Kresno Nugroho <kresnonugroho46@gmail.com>
2026-01-13 15:02:50 +0700

    update:add chart

--- src/pages/produk/data-pasar-realtime.tsx
+++ src/pages/produk/data-pasar-realtime.tsx
@@ -21,6 +21,16 @@ export default function LiveQuotesPage() {
       <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-52 my-10">
         <ProfilContainer title={t("marketTitle")} description={t("marketSubtitle")}>
           <LiveQuotesTable />
+          <div className="mt-8 overflow-hidden rounded-lg border border-gray-200 bg-[#0F0F0F]">
+            <iframe
+              title="nm-chart"
+              allow="fullscreen"
+              allowFullScreen
+              loading="lazy"
+              style={{ width: "100%", height: 520 }}
+              src="https://s.tradingview.com/embed-widget/advanced-chart/?symbol=OANDA:XAUUSD&interval=D&theme=dark&style=1&locale=en&allow_symbol_change=true&hide_side_toolbar=true&hide_top_toolbar=false&hide_legend=false&hide_volume=true&exclude_studies=STD%3BVolume&details=true&autosize=true&backgroundColor=%230F0F0F&gridColor=rgba(242,242,242,0.06)&timezone=Etc/UTC&studies=STD%3BStochastic_RSI"
+            />
+          </div>
         </ProfilContainer>
       </div>
     </PageTemplate>
