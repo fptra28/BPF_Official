81a6f38199f5fa63ecb609a3731fb5cc2f464ea1
Kresno Nugroho <kresnonugroho46@gmail.com>
2026-01-15 08:01:00 +0700

    update:fax

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -38,6 +38,13 @@ const Icons = {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" stroke="currentColor" />
         </svg>
     ),
+    Fax: (props: IconProps) => (
+        <svg {...props} className="h-5 w-5 flex-shrink-0 text-[#FF0000]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 9V4h12v5M6 18H4a2 2 0 01-2-2v-5a2 2 0 012-2h16a2 2 0 012 2v5a2 2 0 01-2 2h-2" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 14H6v6h12v-6z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 12h.01" />
+        </svg>
+    ),
     Document: (props: IconProps) => (
         <svg {...props} className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -144,7 +151,13 @@ const Footer = () => {
                             <div className="flex items-center group">
                                 <Icons.Phone />
                                 <div className="ml-3 text-sm text-black">
-                                    <span>Fax: 021 - 29035132</span>
+                                    <span>Telp: +62 21 2903 5005 (Hunting)</span>
+                                </div>
+                            </div>
+                            <div className="flex items-center group">
+                                <Icons.Fax />
+                                <div className="ml-3 text-sm text-black">
+                                    <span>Fax: +62 21 2903 5132</span>
                                 </div>
                             </div>
                             <div className="flex items-center group">
